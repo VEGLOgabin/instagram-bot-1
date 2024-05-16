c320cf1a39a76451c82b89699b61ef4959071187
VEGLOhouetchenouGABIN <houetchenougabinveglo@gmail.com>
2024-05-16 04:38:11 +0100

    feat: instagram bot 1 update for login part

--- insta-bot1.py
+++ insta-bot1.py
@@ -1,38 +1,102 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementClickInterceptedException
 import time
 
-url = "https://www.instagram.com/wsj/"
+## This instagram was created to scrape follower's profile data
 
-def instagram_profile_scraper():
-    # Initialize WebDriver
-    driver = webdriver.Firefox() 
-    
-    # Open the URL
-    driver.get(url)
-    driver.implicitly_wait(3)
-    time.sleep(2)  
-    
-    try:
+class IntagramBotFollowerScrpaer:
+    def __init__(self, email, username, password):
         
-        followers = driver.find_element(By.CSS_SELECTOR, ".x2pgyrj:nth-child(2) ._ap30")
+        self.email = email
+        self.username = username
+        self.password = password
         
-        print(followers.text)
         
-        posts= driver.find_element(By.CSS_SELECTOR, ".x2pgyrj:nth-child(1) ._ap30").text
-        print(posts)
+        # Initialize WebDriver
+        self.driver = webdriver.Firefox() 
+        # self.driver.get(url)
+        # self.driver.implicitly_wait(3)
+        # time.sleep(3)  
         
-        following= driver.find_element(By.CSS_SELECTOR, ".x2pgyrj~ .x2pgyrj+ .x2pgyrj ._ap30").text
-        print(following)
+    def login(self):
+        self.driver.get('https://www.instagram.com/accounts/login')
+        time.sleep(5)
+        
+        username = self.driver.find_element(By.NAME,'username')
+        password = self.driver.find_element(By.NAME,'password')
+        
+        username.send_keys(self.email)
+        password.send_keys(self.password)
+        
+        time.sleep(2)
+        password.send_keys(Keys.ENTER)
+        
+        
+    def find_user(self):
+        time.sleep(5)
+        self.driver.get(f'https://www.instagram.com/{self.username}')
+        time.sleep(5)
+        
+        followers = self.driver.find_element(By.CSS_SELECTOR,'li a ._ac2a')
+        followers.click()
+        
+    def follow(self, number):
+        time.sleep(5)
+        follow_buttons = []
+        for i in range(1, number + 1):
+            btn = self.driver.find_element(By.XPATH,f'/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[2]/div/div[{i}]/div/div/div/div[3]/div/button')
+            follow_buttons.append(btn)
+            
+        for button in follow_buttons:
+            try:
+                button.click()
+                time.sleep(1)
+            except ElementClickInterceptedException:
+                print("Click has been intercepted by a modal")
     
-    except Exception as e:
-        print("An error occurred:", e)
+        
+
+
+    def scrapeCurrentUserInfos(self):
+        try:
+            
+            followers = self.driver.find_element(By.CSS_SELECTOR, ".x2pgyrj:nth-child(2) ._ap30")
+            
+            print(followers.text)
+            
+            posts= self.driver.find_element(By.CSS_SELECTOR, ".x2pgyrj:nth-child(1) ._ap30").text
+            print(posts)
+            
+            following= self.driver.find_element(By.CSS_SELECTOR, ".x2pgyrj~ .x2pgyrj+ .x2pgyrj ._ap30").text
+            print(following)
+        
+        except Exception as e:
+            print("An error occurred:", e)
+        
+        finally:
+            pass
+        
+        
+    def getFollowersListAndScrapeProfileInfos(self):
+        pass
     
-    finally:
+    
+    def close_driver(self):
         # Close the driver
-        driver.quit()
+        self.driver.quit()
 
-# Call the function to start scraping
-instagram_profile_scraper()
+email = "xxxxxxxxxxxx"
+username = "xxxxxxxxxxxxx"
+password = "xxxxxxxxx"
+
+# bot1 
+bot1 = IntagramBotFollowerScrpaer(email,username,password)
+
+bot1.login()
+bot1.find_user()
+bot1.follow(3)
+bot1.close_driver()
